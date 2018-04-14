f3d2b9f748cdda6ce1adc5e6da726be0db5a8dfc
nehab0130 <neha_vasuki@yahoo.com>
2018-04-14 12:13:57 +0000

    added messages to content

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,23 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            "sender": "USER",
+            "text": "Hello"
+		},
+        {
+            "sender": "BOT",
+            "text": "Hi, How can I help?"
+		},
+        {
+            "sender": "USER",
+            "text": "Get information about Marvel"
+		},
+        {
+            "sender": "BOT",
+            "text": "Searching for Marvel"
+		}
+	];
 
 });
